0429b038ced9b2a28b38b416ee8eb7a5b3d073c6
Miel2000 <fabien.orsn@gmail.com>
2019-11-28 14:48:29 +0100

    App My Sentynel - EasyAdmin MagicBundle

---
+++ website-skeleton/src/Migrations/Version20191126145520.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191126145520 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE collaborateur CHANGE isvalid isvalid TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE societe DROP siteweb');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE collaborateur CHANGE isvalid isvalid TINYINT(1) DEFAULT \'0\'');
+        $this->addSql('ALTER TABLE societe ADD siteweb VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_general_ci`');
+    }
+}
